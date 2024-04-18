9bcbdcd4b95888954a383e70a18ab30278e688b1
David Mejia <jose.david.mejia.meneses@gmail.com>
2024-04-17 19:53:49 -0500

    Cambio de conexión de base de datod de sqlite a postgreSQL

--- app/settings.py
+++ app/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "control_combustible",
+        "USER": "postgres",
+        "PASSWORD": "Popepi2607*12",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
