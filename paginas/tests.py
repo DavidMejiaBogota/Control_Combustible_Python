c219d99506b08933f7350955f9b13709b951a71c
David Mejia <jose.david.mejia.meneses@gmail.com>
2024-05-07 09:15:16 -0500

    Test de plantillas (setUp y optimización)

--- paginas/tests.py
+++ paginas/tests.py
@@ -2,18 +2,14 @@ from django.test import SimpleTestCase
 from django.urls import reverse
 
 class AboutPageTests(SimpleTestCase):
-    def test_existe_ruta(self):
-        response = self.client.get("/pages/about")
-        self.assertEqual(response.status_code, 200)
-    
-    def test_nombre_ruta_about(self):
-        response = self.client.get(reverse("pages:about"))
-        self.assertEqual(response.status_code, 200)
-    
-    def test_plantilla_contiene_html(self):
-        response = self.client.get(reverse("pages:about"))
-        self.assertContains(response, "Aplicación para el control de Combustible")
 
-    def test_plantilla_no_contiene_html(self):
-        response = self.client.get(reverse("pages:about"))
-        self.assertNotContains(response, "Curso J. David Mejía / FB/IG DavidDeveloper")
+    def setUp(self):
+        self.response = self.client.get("/pages/about")
+
+    def test_url_about(self):
+        self.assertEqual(self.response.status_code, 200)
+    
+    def test_html_about(self):
+        self.assertTemplateUsed(self.response, "paginas/about.html")
+        self.assertContains(self.response, "Aplicación para el control de Combustible")
+        self.assertNotContains(self.response, "Curso J. David Mejía / FB/IG DavidDeveloper")
