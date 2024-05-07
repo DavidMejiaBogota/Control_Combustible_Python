449167b27d7bdc3bfd2ca23c1210c1f1bfc36831
David Mejia <jose.david.mejia.meneses@gmail.com>
2024-05-07 16:19:23 -0500

    Creación plantilla Home

--- bases/urls.py
+++ bases/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = "core"
 
 urlpatterns =[
-    path('', primera_vista, name="primera_vista"),
+    #path('', primera_vista, name="primera_vista"),
+    path('', HomeView.as_view(), name="home")
 ]
--- bases/views.py
+++ bases/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import *
 
 def primera_vista(request):
     return HttpResponse("Hola mundo, desde el curso de Django para profesionales con: <b>J. David Mejía M.</b> ")
 
-
+class HomeView(TemplateView):
+    template_name = "bases/home.html"
