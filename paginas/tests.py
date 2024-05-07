d1e55e0d08c9a91f1fd6dfac39c40049ee665346
David Mejia <jose.david.mejia.meneses@gmail.com>
2024-05-06 19:04:06 -0500

    Test SimpleTestCase

--- paginas/tests.py
+++ paginas/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class AboutPageTests(SimpleTestCase):
+    def test_existe_ruta(self):
+        response = self.client.get("/pages/about")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_nombre_ruta_about(self):
+        response = self.client.get(reverse("pages:about"))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_plantilla_contiene_html(self):
+        response = self.client.get(reverse("pages:about"))
+        self.assertContains(response, "Aplicación para el control de Combustible")
+
+    def test_plantilla_no_contiene_html(self):
+        response = self.client.get(reverse("pages:about"))
+        self.assertNotContains(response, "Curso J. David Mejía / FB/IG DavidDeveloper")
