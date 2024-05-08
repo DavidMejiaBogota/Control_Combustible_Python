9f76dbfd706ef415a7e53fa86006f05d7963e229
David Mejia <jose.david.mejia.meneses@gmail.com>
2024-05-08 17:49:41 -0500

    Configuración registro de nuevo usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('login2/',
         auth_views.LoginView.as_view(template_name="usuarios/login.html")
-        ,name="login")
+        ,name="login"),
+    path('registro/', RegistroUsuarioView.as_view(), name="registro"),
 ]
