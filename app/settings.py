bb86ffd016debfe174ff621e77cf116dd6223392
David Mejia <jose.david.mejia.meneses@gmail.com>
2024-05-08 15:38:20 -0500

    Reconfiguarción del login

--- app/settings.py
+++ app/settings.py
@@ -141,3 +141,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'usuarios.Usuario'
+LOGIN_REDIRECT_URL ='core:home'
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 from .views import *
+
 
 app_name = "usuarios"
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('login2/',
+        auth_views.LoginView.as_view(template_name="usuarios/login.html")
+        ,name="login")
 ]
