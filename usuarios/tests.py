7f40e3c427f08f15c4c8fd18518d79fd0a8af653
David Mejia <jose.david.mejia.meneses@gmail.com>
2024-06-03 12:30:22 -0500

    Configuración 5 pruebas de usuario y Crispy Forms

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
+
+from .forms import CustomUserCreationForm
+from .views import RegistroUsuarioView
+
 
 # Create your tests here.
 
@@ -38,3 +43,26 @@ class CustomUserTest(TestCase):
         self.assertTrue(usr.is_active)
         self.assertTrue(usr.is_staff)
         self.assertTrue(usr.is_superuser)
+
+class RegistroUsuarioTest(TestCase):
+    def setUp(self):
+        url = reverse("usuarios:registro")
+        self.response = self.client.get(url)
+    
+    #Prueba de plantilla
+    def test_plantilla_registro(self):
+        self.assertEqual(self.response.status_code,200)
+        self.assertTemplateUsed(self.response,"usuarios/registro.html")
+        self.assertContains(self.response,"Registrate")
+        self.assertNotContains(self.response,"Bienvenido")
+    
+    #Prueba del formulario
+    def test_registro_form(self):
+        form = self.response.context.get("form")
+        self.assertIsInstance(form,CustomUserCreationForm)
+        self.assertContains(self.response,"csrfmiddlewaretoken")
+
+    #Pruebas de vista
+    def test_registro_vista(self):
+        view = resolve("/usuarios/registro/")
+        self.assertEqual(view.func.__name__,RegistroUsuarioView.as_view().__name__)
