4109461c231aa8d18fab936896791dfdf801069e
David Mejia <jose.david.mejia.meneses@gmail.com>
2024-07-03 14:13:15 -0500

    Agregar/crear registros de Marca

--- ctrl_comb/urls.py
+++ ctrl_comb/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('mark/',MarkList.as_view(),name="mark_list"),
+    path("mark/save",mark_save,name="mark_save"),
 ]
--- ctrl_comb/views.py
+++ ctrl_comb/views.py
@@ -8,3 +8,17 @@ class MarkList(ListView):
     model=Mark
     context_object_name="obj"
     ordering=["descript"]
+
+def mark_save(request):
+    context={}
+    template_name= "ctrl_comb/mark-list.html"
+
+    if request.method=="POST":
+        i = request.POST.get("id")
+        d = request.POST.get("descript")
+
+        o = Mark.objects.create(descript = d)
+    obj = Mark.objects.all().order_by("descript")
+    context["obj"] = obj
+
+    return render(request,template_name,context)
