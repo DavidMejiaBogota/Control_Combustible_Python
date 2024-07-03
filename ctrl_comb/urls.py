ad4a858f03d056b7ceec1caad80e1cc92787004c
David Mejia <jose.david.mejia.meneses@gmail.com>
2024-07-03 14:33:47 -0500

    Barrar registros del model Marca

--- ctrl_comb/urls.py
+++ ctrl_comb/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('mark/',MarkList.as_view(),name="mark_list"),
     path("mark/save",mark_save,name="mark_save"),
+    path("mark/delete/<int:pk>",mark_delete,name="mark_delete"),
 ]
