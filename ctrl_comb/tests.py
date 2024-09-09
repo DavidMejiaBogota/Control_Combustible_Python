34341dfa1e7e7ed13108bb8b616a34c00119175d
David Mejia <jose.david.mejia.meneses@gmail.com>
2024-09-09 10:27:57 -0500

    Ajuste a las pruebas

--- ctrl_comb/tests.py
+++ ctrl_comb/tests.py
@@ -15,24 +15,20 @@ class ModeloTest(TestCase):
         self.assertEqual(self.modelo.descript,"Rush")
         self.assertEqual(self.modelo.mark.descript,"Toyota")
     
+    def test_vista_modelo(self):
+        response = self.client.get(reverse("control:modelo_list"))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,"Rush")
+        self.assertTemplateUsed(response,"ctrl_comb/modelo.html")
+    
+    def test_vista_modelo(self):
+        response = self.client.get(reverse("control:mark_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,"Toyota")
+        self.assertTemplateUsed(response,"ctrl_comb/mark.html")
+            
     def test_vista_modal(self):
         response = self.client.get(reverse("control:modelo_edit_modal",kwargs={'pk':self.modelo.id}))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response,"Rush")
         self.assertTemplateUsed(response,"ctrl_comb/modelo_modal.html")
-
-    def test_vista_modelo(self):
-        response = self.client.get(reverse("control:modelo_list"))
-        self.assertEqual(response.status_code,200)
-        self.assertContains(response,"Rush")
-        self.assertTemplateUsed(response,"ctrl_comb/mark_list.html")
-        
-"""
-    def test_vista_modelo(self):
-        response = self.client.get(reverse("control:mark_list"))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response,"Toyota")
-        self.assertTemplateUsed(response,"ctrl_comb/mark_list.html")
-    
-
-"""
